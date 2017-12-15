506518232b73dfaf1f8691ab7005b881988a956b
Sravan Karuturi <Sravan Karuturi@Home>
2017-12-14 23:12:09 -0500

    Dumping memory leaks

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include "Engine.h"
 
+#ifdef _DEBUG
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#endif // _DEBUG
+
+
 int main() {
+
+#ifdef _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+#endif // _DEBUG
+
 	
 	// No Actual use here.. using the unique ptr, except that maybe it cleans itself up when exiting the program.
 	Engine e = (Engine::getInstance());
